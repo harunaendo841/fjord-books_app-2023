78047aa42ca1a73d7004705d4b05f7838b4a094c
HarunaHatanaka <h.hatanaka@genbasupport.com>
2025-06-05 15:44:03 +0900

    ユーザー認証機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: '/letter_opener'
+  end
+  devise_for :users
+  resources :users, only: %i[index show]
   resources :books
+
+  authenticated :user do
+    root to: 'books#index', as: :authenticated_root
+  end
+  root to: 'books#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
