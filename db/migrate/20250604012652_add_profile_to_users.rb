f3bf0c49b790ae463ae23e5b15fcf4bcbd53116e
HarunaHatanaka <h.hatanaka@genbasupport.com>
2025-06-29 14:22:40 +0900

    postal_codeとaddressフィールドにnull:false制約を追加

--- db/migrate/20250604012652_add_profile_to_users.rb
+++ db/migrate/20250604012652_add_profile_to_users.rb
@@ -1,7 +1,7 @@
 class AddProfileToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :postal_code, :string
-    add_column :users, :address, :string
+    add_column :users, :postal_code, :string, null: false
+    add_column :users, :address, :string, null: false
     add_column :users, :bio, :text
   end
 end
